fe4e89a0866ec92b0e1b81d759568e43b7bd8846
Tsoi Wai Mun <waimun.t@nanolumi.com>
2026-03-22 15:58:39 +0800

    Complete Task 4 Redux integration in cart components

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -32,7 +32,11 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    if (item.quantity >= 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+    } else {
+      dispatch(addItem(item));
+    }
   };
 
   const handleDecrement = (item) => {
